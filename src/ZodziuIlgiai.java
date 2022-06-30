d5fa66d220f9ff064eb5b5bf76caca4332197bde
saulgiedr1 <saulgiedr@gmail.com>
2022-06-30 19:57:54 +0300

    zodziai to int

---
+++ src/ZodziuIlgiai.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ZodziuIlgiai {
+    public static void main(String[] args) {
+
+        ArrayList<String> zodis = new ArrayList<>();
+
+        zodis.add("namas");
+        zodis.add("lova");
+        zodis.add("kaimas");
+        zodis.add("miestas");
+        zodis.add("langas");
+        zodis.add("varna");
+
+        System.out.println("zodziu listas yra: " + zodis);
+
+
+    }
+    public static Integer zodziuIlgis(ArrayList<String > zodis){
+
+    }
+}

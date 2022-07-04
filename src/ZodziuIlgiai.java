d23f4f0ca6e7b7171533759003ec81cb93240c12
saulgiedr1 <saulgiedr@gmail.com>
2022-07-04 14:34:57 +0300

    Baigta uzduotis su zodziu ilgiais

--- src/ZodziuIlgiai.java
+++ src/ZodziuIlgiai.java
@@ -10,20 +10,38 @@ public class ZodziuIlgiai {
         zodis.add("kaimas");
         zodis.add("miestas");
         zodis.add("langas");
-        zodis.add("varna");
+        zodis.add("varnalesa");
+        zodis.add("furgonas");
 
         System.out.println("zodziu listas yra: " +zodis);
-        System.out.println("zodziai skaicius = " + zodis.size());
+        System.out.println("zodziu skaicius = " + zodis.size());
 
+        ArrayList<Integer> ilgis=zodziuIlgis(zodis);
+        System.out.println("Zodziu ilgiai = " + ilgis);
 
+        int skaitmuo = 8;
 
-
-
-
-
-
+        System.out.println("Ilgesni uz paduodama skaitmeni : "+ didesniUz(ilgis,skaitmuo));
 
     }
+    public static ArrayList<Integer> zodziuIlgis(ArrayList<String> zodis){
+        ArrayList<Integer> ilgiai = new ArrayList<>();
+        for (int i=0; i < zodis.size(); i++){
+            zodis.get(i).length();
+            ilgiai.add(zodis.get(i).length());
+        }
+        return ilgiai;
+    }
+    public static int didesniUz(ArrayList<Integer> ilgis , int x) {
+        int didesni = 0;
+        for (int i = 0; i < ilgis.size(); i++) {
+            if (ilgis.get(i) > x) {
+                didesni++;
+            }
+        }
+        return didesni;
+    }
+
 
 
 }
